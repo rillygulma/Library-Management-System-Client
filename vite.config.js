e7cbb3612d09e0dd11efeb3d96876a1313a6bf72
Rilwanu Idris <Rillygulma@gmail.com>
2024-07-29 23:28:46 +0100

    add netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000, // Specify the port number
-    host: '0.0.0.0', // Expose the server to external network
-  },
-  // other configurations
+  plugins: [react()],
+  base: '/', // Ensure base path is set to the root
 });
